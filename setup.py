728607cf5c0a2fe545185d4184012636e1228d87
Gregory Rehm <grehm87@gmail.com>
2014-12-18 01:03:07 -0800

    first commit for basic file server

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="stepping_stones",
+    version="0.1",
+    description="Basic file upload server using django",
+    install_requires=[
+        'django',
+    ]
+)
---
+++ stepping_stones/stepping_stones/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'stepping_stones.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'stepping_stones.views.index', name='index'),
+)
